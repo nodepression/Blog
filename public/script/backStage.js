74e110ea9fedf1f4bb3f1132c0b43a149ddd3202
zq <qianzgcn@163.com>
2017-11-27 14:18:48 +0800

    博客管理系统前端初步完成

--- public/script/backStage.js
+++ public/script/backStage.js
@@ -2,35 +2,30 @@
     var configure = {};//要发布文章的参数
 
     function getConfigure(){
-        configure.title = $(".title").html();
-        configure.timestamp = new Date().getTime();
-        configure.type = $(".type").html();
-        configure.head = 1;
+        configure.title = $.trim($(".title").val());//文章标题
+        configure.timestamp = new Date().getTime();//发布时间戳
+        configure.type = ["js","c/c++","人文"];//文章类型
+        configure.intro = $.trim($(".article_intro").val());;//文章简短介绍
+        configure.article = $("#result").html(); //文章正文
     }
-
     //markdown转html
     function compile() {
         var text = document.getElementById("content").value;
         var converter = new showdown.Converter();
         var html = converter.makeHtml(text);
-        console.log(html)
+        // console.log(html)
         document.getElementById("result").innerHTML = html;
     }
 
     //发布文章
     function publish(configure) {
-        var timestamp = new Date().getTime(),
-            title = configure.title,
-            head = configure.head,
-            type = configure.type;
-
         $.ajax({
             type: "POST",
             url: "/backStage/publish",
             data: configure,
             dataType: 'json',
             success: function (myData) {
-
+                alert(myData.data.state);
             },
             error: function (xhr, type) {
                 console.log(type);
@@ -44,9 +39,15 @@
 
 
 
-    // 事件绑定
+    // markdown转换
     $("#content").keyup(function () {
         compile();
+    })
+
+    //发布文章
+    $(".publish").click(function () {
+        getConfigure();//得到准备发布的文章的参数
+        publish(configure);
     })
 
 }.call(this));
